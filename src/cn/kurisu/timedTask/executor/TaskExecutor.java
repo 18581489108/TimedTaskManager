2b989dc1617e4b68a26a649a0505cddb2a251926
kurisu <18581489108@163.com>
2017-02-06 21:06:15 +0800

    新增任务执行器，具体代码未实现

---
+++ src/cn/kurisu/timedTask/executor/TaskExecutor.java
@@ -0,0 +1,24 @@
+package cn.kurisu.timedTask.executor;
+
+import cn.kurisu.bean.timedTask.YTask;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by ym on 2017/2/6 0006.
+ *
+ * 任务执行器
+ * 加入执行器的任务不允许撤销
+ */
+public interface TaskExecutor {
+    /**
+     * 加入任务
+     * */
+    void addTask(YTask task);
+
+    /**
+     * 加入一系列任务
+     * */
+    void addTasks(Collection<YTask> tasks);
+}
---
+++ src/cn/kurisu/timedTask/executor/impl/TimedTaskExecutor.java
@@ -0,0 +1,16 @@
+package cn.kurisu.timedTask.executor.impl;
+
+import cn.kurisu.bean.timedTask.YTask;
+import cn.kurisu.timedTask.executor.TaskExecutor;
+
+/**
+ * Created by ym on 2017/2/6 0006.
+ *
+ * 定时任务执行者s
+ */
+public class TimedTaskExecutor implements TaskExecutor {
+    @Override
+    public void addTask(YTask task) {
+
+    }
+}
